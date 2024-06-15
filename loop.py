3470018c99214246d4592ecd33ee4b6df21aaac6
adnan farooqui <adnanfarooqui2000@gmail.com>
2024-06-15 18:51:37 +0530

    second commit

--- loop.py
+++ loop.py
@@ -5,7 +5,7 @@ for i in range(2,20,2):
 '''
 # Difference between break & return
 # break keyword particular block se bhar nikal deta hai
-#return keyword 
+# return keyword 
 def f(n):
     for i in range(1,n+1):
         yield i
